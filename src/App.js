07f888a5e2782d89bf262c1c45bf5dd3a14fac5c
tahirhameed99 <tahir.hameed99@gmail.com>
2019-11-19 16:52:06 +0500

    POST API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,78 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+          error: null,
+          isLoaded: false,
+          items: [],
+          flag:0
+        };
+  }
+
+MyPOSTAPI=()=>{
+
+  // var registerDict = {name:'akhzar',email:'akhzarn@yahoo.com',pass:'123456'};
+
+  var dict = { title: 'foo', body: 'bar', userId: 1 };
+
+  axios.post('https://jsonplaceholder.typicode.com/posts',dict)
+  .then(response => {
+    console.log(response)
+    this.setState({isLoaded:true,
+                    flag:1
+                  })
+  })
+  .catch(error => {
+    alert(error);
+    console.log(error);
+  })
+
+}
+
+  render() {
+
+    const { error, isLoaded, items } = this.state;
+
+    if (this.state.flag == 0){
+
+      return(
+      <div>
+
+      <button onClick={this.MyPOSTAPI}>
+      Register
+      </button>
+
+      </div>
+
+      );
+
+    }else{
+
+      return(
+
+      <div>
+      <button onClick={this.MyPOSTAPI}>
+      Register
+      </button>
+
+      <h1> Akhzar Post Request Submitted Successfully</h1>
+
+      </div>
+
+      );
+    }
+
+  }
+
+  componentDidMount() {
+
+    }
 }
 
 export default App;
